c738ed3017557a560960f4ec78946da312851b7d
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2022-12-20 15:54:47 -0300

    Preparing gh-pages branch

--- Sources/swift-bezier/Internal/Comparable+Ext.swift
+++
@@ -1,6 +0,0 @@
-extension Comparable {
-    @inlinable
-    func clamp(min minimum: Self, max maximum: Self) -> Self {
-        min(max(self, minimum), maximum)
-    }
-}
--- Sources/swift-bezier/Protocols/Point/Bezier2PointType.swift
+++
@@ -1,8 +0,0 @@
-/// A 2-dimensional Bézier point type.
-public protocol Bezier2PointType: BezierPointType {
-    /// The X component of this point.
-    var x: Scalar { get }
-
-    /// The Y component of this point.
-    var y: Scalar { get }
-}
